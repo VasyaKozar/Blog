f055071231d0740d58e7632d31c0275e91d9feb6
KV <vasyakozar13@gmail.com>
2018-10-12 20:41:57 +0300

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  
+def new
+	@post = Post.new
+end
+ 	def create
+ 		@post = Post.new(post_params)
+ 		if @post.save
+ 			redirect_to root_path
+ 		end
+ 	end
+ 	def post_params
+ 		params.require(:post).permit(:username, :title, :body)
+ 	end
+  def index
+  end
+end
